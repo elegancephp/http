1cbbf070056c67a6ff312a97088d9ab24617123c
André Ricardo <contatoadmx@gmail.com>
2022-12-07 12:11:10 -0300

    view prepare url

--- helper/script/view_prepare.php
+++ helper/script/view_prepare.php
@@ -5,3 +5,5 @@ use Elegance\View;
 View::prepare('view', fn ($viweRef) => view($viweRef));
 
 View::prepare('this.view', fn ($viweRef) => viewIn($viweRef));
+
+View::prepare('url', fn () => url(...func_get_args()));
