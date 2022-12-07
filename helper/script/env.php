dc55e384590ebdc3234bb7d24930b991af0fff95
André Ricardo <contatoadmx@gmail.com>
2022-12-07 14:48:15 -0300

    remove cif

--- helper/script/env.php
+++ helper/script/env.php
@@ -4,10 +4,6 @@ use Elegance\Env;
 
 Env::default('SERVER_PORT', 8081);
 
-Env::default('PATH_CIF', 'library/certificate');
-
-Env::default('CODE_KEY', 'elegnace');
-
 
 Env::default('RELATIVE_METHOD', true);
 
