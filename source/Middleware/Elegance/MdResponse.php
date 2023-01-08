9db15ca9f18a8f6431f942e92692237847e38948
André Ricardo <contatoadmx@gmail.com>
2023-01-07 21:29:35 -0300

    auto response json and array

--- source/Middleware/Elegance/MdResponse.php
+++ source/Middleware/Elegance/MdResponse.php
@@ -15,39 +15,27 @@ class MdResponse
     {
         try {
             $reponse = $next();
-
             if (is_int($reponse) && $reponse >= 200 && $reponse <= 599)
-                $reponse = $this->getResponseStatus($reponse, 'friendly');
-
-            if (is_array($reponse))
+                throw new Exception('friendly', $reponse);
+            if (is_array($reponse) || is_json($reponse))
                 $reponse = (new Response($reponse))->type('json');
-
             $reponse = new Response($reponse);
         } catch (InputException $e) {
-            $reponse = $this->getResponseStatus($e->getCode(), $e->getMessage());
+            $reponse = new Response();
+            $reponse->status($this->getHttpStatus($e->getCode(), STS_BAD_REQUEST));
             $reponse->type('json');
             $reponse->content($e->getMessage());
         } catch (Exception | Error $e) {
-            $reponse = $this->getResponseStatus($e->getCode(), $e->getMessage());
+            $reponse = new Response();
+            $reponse->status($this->getHttpStatus($e->getCode(), STS_BAD_REQUEST));
+            $reponse->header('El-Status-Code', $e->getCode());
+            $reponse->header('El-Status-Message', $e->getMessage());
         }
         return $reponse;
     }
 
     protected function getHttpStatus(int $code, int $default): int
     {
-        if ($code >= 200 && $code <= 599) return $code;
-        return $default;
-    }
-
-    protected function getResponseStatus(int $code, string $message): Response
-    {
-        $reponse = new Response();
-
-        $reponse->status($this->getHttpStatus($code, STS_INTERNAL_SERVER_ERROR));
-
-        $reponse->header('El-Status-Code', $code);
-        $reponse->header('El-Status-Message', $message);
-
-        return $reponse;
+        return num_interval($code, 200, 599) == $code ? $code : $default;
     }
 }
